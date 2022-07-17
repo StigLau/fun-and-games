31e8a204a2ddb3eb8b277d2aec43032cc98491b9
stiglau <stig@lau.no>
2022-07-17 20:33:20 +0200

    Initial commit

---
+++ loomin/src/main/java/no/lau/LoominCrazy.java
@@ -0,0 +1,13 @@
+package no.lau;
+
+/**
+ * Hello world!
+ *
+ */
+public class LoominCrazy
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
