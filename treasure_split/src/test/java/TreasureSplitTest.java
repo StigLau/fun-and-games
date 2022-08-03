73095ea836fd39a70323883b9d2a5a629a854dd0
stiglau <stig@lau.no>
2022-08-03 19:09:59 +0200

    Removing uneccessary list

--- treasure_split/src/test/java/TreasureSplitTest.java
+++ treasure_split/src/test/java/TreasureSplitTest.java
@@ -11,33 +11,33 @@ public class TreasureSplitTest {
 
     @Test
     public void testFirstVersion() {
-        Map<Integer, List<Integer>> result = doStuff(List.of(1, 2, 3), List.of(4, 4, 4) );
+        Map<Integer, List<Integer>> result = doStuff(3, List.of(4, 4, 4) );
 
         //Assume bag is sorted
 
         assertEquals(result.size(), 3);
+        assertEquals(4, result.get(0).get(0));
         assertEquals(4, result.get(1).get(0));
         assertEquals(4, result.get(2).get(0));
-        assertEquals(4, result.get(3).get(0));
 
     }
 
-    public Map<Integer, List<Integer>> doStuff(List<Integer> hunters, List<Integer> gems) {
+    public Map<Integer, List<Integer>> doStuff(int nrOfHunters, List<Integer> gems) {
         //Initialize
         Map<Integer, List<Integer>> result = new HashMap<>();
-        for (Integer key : hunters) {
-            result.put(key, new ArrayList<>());
+        for (int i = 0; i < nrOfHunters; i++) {
+            result.put(i, new ArrayList<>());
         }
 
         //Compute
-        int hunterRef = 1;
+        int hunterRef = 0;
         for (Integer gem : gems) {
             result.get(hunterRef).add(gem);
 
-            if(hunterRef < hunters.size()) {
+            if(hunterRef < result.size()) {
                 hunterRef++;
             } else {
-                hunterRef = 1;
+                hunterRef = 0;
             }
         }
         return result;
